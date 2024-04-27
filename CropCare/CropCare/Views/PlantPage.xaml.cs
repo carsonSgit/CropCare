1e6592bf10d1d9dd30eeb49eba7c928b109519fd
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-04-27 18:50:19 -0400

    Setting the Plant labels to use mock data

--- CropCare/CropCare/Views/PlantPage.xaml.cs
+++ CropCare/CropCare/Views/PlantPage.xaml.cs
@@ -1,14 +1,74 @@
 using CropCare.Models;
+using CropCare.Models.Plant;
 
 namespace CropCare.Views;
 
 public partial class PlantPage : ContentPage
 {
 	private Farm Farm { get; set; }
+    public PlantController PlantController { get; set; }
+    public string Temperature
+    {
+        get
+        {
+            var reading = PlantController.Temperature.ReadSensor()[0];
+            return $"{reading.Value}{reading.Unit}";
+        }
+    }
+
+    public string Humidity
+    {
+        get
+        {
+            var reading = PlantController.Temperature.ReadSensor()[1];
+            return $"{reading.Value}{reading.Unit}";
+        }
+    }
+
+    public string Moisture
+    {
+        get
+        {
+            var reading = PlantController.SoilMoisture.ReadSensor()[0];
+            return $"{reading.Value}{reading.Unit}";
+        }
+    }
+    public string WaterLevel
+    {
+        get
+        {
+            var reading = PlantController.WaterLevel.ReadSensor()[0];
+            return $"{reading.Value}{reading.Unit}";
+        }
+    }
+
+    public string Led { 
+        get { return PlantController.Led.State; }
+        set { PlantController.Led.State = value; }
+    }
+    public string Fan
+    {
+        get { return PlantController.Fan.State; }
+        set { PlantController.Fan.State = value; }
+    }
     public PlantPage(Farm farm)
 	{
 		InitializeComponent();
         Farm = farm;
+        PlantController = new PlantController();
         BindingContext = farm;
+
+        SetMeasurements();
+    }
+
+    public void SetMeasurements()
+    {
+        temperatureLbl.Text = Temperature;
+        humidityLbl.Text = Humidity;
+        moistureLbl.Text = Moisture;
+        waterlvlLbl.Text = WaterLevel;
+
+        ledLbl.Text = Led;
+        fanLbl.Text = Fan;
     }
 }
