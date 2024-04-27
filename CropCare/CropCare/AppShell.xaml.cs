3e90a4e99136d7b20d06a10a1b56a6295a7a764f
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-04-27 11:56:00 -0400

    Setup logout command in menu

--- CropCare/CropCare/AppShell.xaml.cs
+++ CropCare/CropCare/AppShell.xaml.cs
@@ -1,14 +1,23 @@
-﻿namespace CropCare
+﻿using System.Windows.Input;
+
+namespace CropCare
 {
     public partial class AppShell : Shell
     {
+        public ICommand LogoutCommand { get; }
+
         public AppShell()
         {
             InitializeComponent();
+            LogoutCommand = new Command(OnLogout);
 
             // Set the app theme based on the user's preference
             var appTheme = Preferences.Get("apptheme", true);
             App.Current.UserAppTheme = appTheme ? AppTheme.Light : AppTheme.Dark;
+        }
+
+        private void OnLogout()
+        {
         }
     }
 }
