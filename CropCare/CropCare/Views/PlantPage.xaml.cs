29ba0f53a6aea534ee8902f61ab4c8ab13c701d7
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-04-28 13:50:37 -0400

    Remodel Plant Page to use Data Binding

--- CropCare/CropCare/Views/PlantPage.xaml.cs
+++ CropCare/CropCare/Views/PlantPage.xaml.cs
@@ -15,48 +15,20 @@ public partial class PlantPage : ContentPage
         InitializeComponent();
         Farm = farm;
         PlantController = new PlantController();
-        BindingContext = Farm;
-
-        SetMeasurements();
-        SetHealth();
-    }
-
-    public void SetMeasurements()
-    {
-        temperature_measurementLbl.Text = PlantController.GetTemperatureReading();
-        humidity_measurementLbl.Text = PlantController.GetHumidityReading();
-        moisture_measurementLbl.Text = PlantController.GetMoistureReading();
-        waterlvl_measurementLbl.Text = PlantController.GetWaterLevelReading();
-    }
-
-    public void SetHealth()
-    {
-        PlantController.UpdateReadingHealthLabel(PlantController.GetTemperatureReading(), temperature_healthLbl, '°', 30, 10);
-        PlantController.UpdateReadingHealthLabel(PlantController.GetHumidityReading(), humidity_healthLbl, '%', 70, 30);
-        PlantController.UpdateReadingHealthLabel(PlantController.GetMoistureReading(), moisture_healthLbl, 'o', 700, 200);
-        PlantController.UpdateReadingHealthLabel(PlantController.GetWaterLevelReading(), waterlvl_healthLbl, 'w', 70, 30);
-
-        PlantController.UpdateStateHealthLabel(PlantController.Led.State, led_healthLbl);
-        PlantController.UpdateStateHealthLabel(PlantController.Fan.State, fan_healthLbl);
+        BindingContext = PlantController;
     }
 
     private void fanSwitch_Toggled(object sender, ToggledEventArgs e)
     {
-        if(fanSwitch.IsToggled)
-            PlantController.Fan.ControlActuator(Models.Command.ON);
-        else
-            PlantController.Fan.ControlActuator(Models.Command.OFF);
-
-        PlantController.UpdateStateHealthLabel(PlantController.Fan.State, fan_healthLbl);
+        var command = fanSwitch.IsToggled ? Models.Command.ON : Models.Command.OFF;
+        PlantController.Fan.ControlActuator(command);
+        PlantController.FanState = PlantController.UpdateStateHealthLabel(command.ToString());
     }
 
     private void ledSwitch_Toggled(object sender, ToggledEventArgs e)
     {
-        if (ledSwitch.IsToggled)
-            PlantController.Led.ControlActuator(Models.Command.ON);
-        else
-            PlantController.Led.ControlActuator(Models.Command.OFF);
-
-        PlantController.UpdateStateHealthLabel(PlantController.Led.State, led_healthLbl);
+        var command = ledSwitch.IsToggled ? Models.Command.ON : Models.Command.OFF;
+        PlantController.Led.ControlActuator(command);
+        PlantController.LedState = PlantController.UpdateStateHealthLabel(command.ToString());
     }
 }
