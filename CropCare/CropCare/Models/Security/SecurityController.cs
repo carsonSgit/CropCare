b31edeca1a5c1856b7ab10c0c81709187b6f907f
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2024-04-28 14:28:45 -0400

    Shell Security Page + Near complete readings

--- CropCare/CropCare/Models/Security/SecurityController.cs
+++ CropCare/CropCare/Models/Security/SecurityController.cs
@@ -2,18 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CropCare.Interfaces;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace CropCare.Models.Security
 {
-    public class SecurityController
+    public class SecurityController : INotifyPropertyChanged
     {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
         public Loudness Loudness { get; set; }
         public Motion Motion { get; set; }
         public Vibration Vibration { get; set; }
         public DoorLock DoorLock { get; set; }
         public DoorOpener DoorOpener { get; set; }
         public Luminosity Luminosity { get; set; }
+        public string LuminosityReading => GetLuminosityReading();
+        public string LuminosityHealth => UpdateReadingHealthLabel(LuminosityReading, 'n', 1000, 500);
+        public string LoudnessReading => GetLoudnessReading();
+        public string LoudnessHealth => UpdateReadingHealthLabel(LoudnessReading, 'u', 1000, 500);
+        public string MotionReading => GetMotionReading();
+        public string MotionHealth => UpdateReadingHealthLabel(MotionReading, ' ', 1000, 500);
+        public string VibrationReading => GetVibrationReading();
+        public string VibrationHealth => UpdateReadingHealthLabel(VibrationReading, ' ', 1000, 500);
+
+        public string DoorLockState
+        {
+            get => GetDoorLockReading();
+            set
+            {
+                if (DoorLock.State != value)
+                {
+                    DoorLock.State = value;
+                    OnPropertyChanged(nameof(DoorLockState));
+                }
+            }
+        }
+        private string _doorOpenerState;
+        public string DoorOpenerState
+        {
+            get => _doorOpenerState;
+            set
+            {
+                if (_doorOpenerState != value)
+                {
+                    _doorOpenerState = value;
+                    OnPropertyChanged(nameof(DoorOpenerState));
+                }
+            }
+        }
+
 
         public SecurityController()
         {
@@ -23,6 +66,63 @@ namespace CropCare.Models.Security
             DoorLock = new DoorLock();
             DoorOpener = new DoorOpener();
             Luminosity = new Luminosity();
+
+            DoorOpenerState = UpdateStateHealthLabel(DoorOpener.State);
+            DoorLockState = UpdateStateHealthLabel(DoorLock.State);
+        }
+
+        public string GetLoudnessReading() => GetSensorReading(Loudness, ReadingType.LOUDNESS);
+        public string GetLuminosityReading() => GetSensorReading(Luminosity, ReadingType.LUMINOSITY);
+        public string GetMotionReading() => GetSensorReading(Motion, ReadingType.MOTION);
+        public string GetVibrationReading() => GetSensorReading(Vibration, ReadingType.VIBRATION);
+        public string GetDoorLockReading() => GetSensorReading(DoorLock, ReadingType.DOORLOCK);
+
+        private string GetSensorReading<T>(T sensor, string readingType) where T : ISensor
+        {
+            var reading = sensor.ReadSensor().FirstOrDefault(r => r.Type.Equals(readingType, StringComparison.OrdinalIgnoreCase));
+            return reading != null ? $"{reading.Value}{reading.Unit}" : "N/A";
+        }
+
+        public string UpdateReadingHealthLabel(string sensorReading, char unitSymbol, double highThreshold, double lowThreshold)
+        {
+            string health = "";
+            double sensorValue;
+            if (double.TryParse(sensorReading.Split(unitSymbol)[0], out sensorValue))
+            {
+                if (sensorValue > highThreshold)
+                {
+                    health = "Critical";
+                    //healthLbl.TextColor = Colors.Red;
+                }
+                else if (sensorValue < lowThreshold)
+                {
+                    health = "Needs Attention";
+                    //healthLbl.TextColor = Colors.Red;
+                }
+                else
+                {
+                    health = "Healthy";
+                    //healthLbl.TextColor = Colors.Green;
+                }
+            }
+
+            return health;
+        }
+
+        public string UpdateStateHealthLabel(string actuatorState)
+        {
+            string health = "";
+            if (actuatorState.Equals(Command.ON.ToString(), StringComparison.OrdinalIgnoreCase))
+            {
+                health = "On";
+                //healthLbl.TextColor = Colors.Green;
+            }
+            else if (actuatorState.Equals(Command.OFF.ToString(), StringComparison.OrdinalIgnoreCase))
+            {
+                health = "Off";
+                //healthLbl.TextColor = Colors.Red;
+            }
+            return health;
         }
     }
 }
--- CropCare/CropCare/Views/SecurityPage.xaml.cs
+++ CropCare/CropCare/Views/SecurityPage.xaml.cs
@@ -1,14 +1,31 @@
 using CropCare.Models;
+using CropCare.Models.Security;
 
 namespace CropCare.Views;
 
 public partial class SecurityPage : ContentPage
 {
     private Farm Farm { get; set; }
+    private SecurityController SecurityController { get; set; } 
     public SecurityPage(Farm farm)
 	{
 		InitializeComponent();
         Farm = farm;
-        BindingContext = farm;
+        SecurityController = new SecurityController();
+        BindingContext = SecurityController;
+    }
+
+    private void doorLockSwitch_Toggled(object sender, ToggledEventArgs e)
+    {
+        var command = doorLockSwitch.IsToggled ? Models.Command.ON : Models.Command.OFF;
+        SecurityController.DoorLock.ControlActuator(command);
+        SecurityController.DoorLockState = SecurityController.UpdateStateHealthLabel(command.ToString());
+    }
+
+    private void doorOpenSwitch_Toggled(object sender, ToggledEventArgs e)
+    {
+        var command = doorOpenSwitch.IsToggled ? Models.Command.ON : Models.Command.OFF;
+        SecurityController.DoorOpener.ControlActuator(command);
+        SecurityController.DoorOpenerState = SecurityController.UpdateStateHealthLabel(command.ToString());
     }
 }
