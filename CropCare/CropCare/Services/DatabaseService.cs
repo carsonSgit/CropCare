97e934862d42534eba325265033c9d7be4a63fb1
Cristiano Fazi <cristianofazi03@gmail.com>
2024-04-28 09:42:19 -0400

    Add Technician Page Completed

--- CropCare/CropCare/Services/DatabaseService.cs
+++ CropCare/CropCare/Services/DatabaseService.cs
@@ -1,12 +1,7 @@
 ﻿using CropCare.Interfaces;
 using Firebase.Database;
 using Firebase.Database.Offline;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CropCare.Services
 {
@@ -17,13 +12,14 @@ namespace CropCare.Services
         {
             get
             {
-                Task.Run(() => LoadItems()).Wait();
+                if (_items == null)
+                    Task.Run(() => LoadItems()).Wait();
                 return _items;
             }
         }
         private async Task LoadItems()
         {
-            _items = new ObservableCollection<T>(await GetItemsAsync(true));
+            _items = new ObservableCollection<T>(await GetItemsAsync());
         }
 
         private readonly RealtimeDatabase<T> _realtimeDb;
@@ -39,6 +35,8 @@ namespace CropCare.Services
             _realtimeDb =
                 client.Child(path)
                 .AsRealtimeDatabase<T>(customKey, "", StreamingOptions.LatestOnly, InitialPullStrategy.MissingOnly, true);
+
+
         }
 
         public async Task<bool> AddItemAsync(T item)
@@ -103,7 +101,7 @@ namespace CropCare.Services
             }
             var result = _realtimeDb.Once().FirstOrDefault(x => key == x.Key);
             return await Task.FromResult(result.Object);
-        }   
+        }
 
         public async Task<bool> UpdateItemAsync(T item)
         {
