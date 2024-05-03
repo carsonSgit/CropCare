8a1f5a64205e8f94f020eadaa634ded50ebc0fed
Carson <92652800+carsonSgit@users.noreply.github.com>
2024-05-03 09:31:11 -0400

    Generalize Security GetSensorReading

--- CropCare/CropCare/Models/Security/SecurityController.cs
+++ CropCare/CropCare/Models/Security/SecurityController.cs
@@ -167,13 +167,13 @@ namespace CropCare.Models.Security
         /// Gets the current reading of motion.
         /// </summary>
         /// <returns>The current motion reading.</returns>
-        public string GetMotionReading() => GetSensorReadingReference(Motion, ReadingType.MOTION);
+        public string GetMotionReading() => GetSensorReading(Motion, ReadingType.MOTION);
 
         /// <summary>
         /// Gets the current reading of vibration.
         /// </summary>
         /// <returns>The current vibration reading.</returns>
-        public string GetVibrationReading() => GetSensorReadingReference(Vibration, ReadingType.VIBRATION);
+        public string GetVibrationReading() => GetSensorReading(Vibration, ReadingType.VIBRATION);
 
         /// <summary>
         /// Gets the current reading of the door lock.
@@ -181,19 +181,11 @@ namespace CropCare.Models.Security
         /// <returns>The current door lock reading.</returns>
         public string GetDoorLockReading() => DoorLock.ReadSensor().FirstOrDefault()?.Value;
 
-
-        private string GetSensorReading<T>(ISensor<T> sensor, string readingType) where T : struct
+        private string GetSensorReading<T>(ISensor<T> sensor, string readingType)
         {
             var reading = sensor.ReadSensor().FirstOrDefault(r => r.Type.Equals(readingType, StringComparison.OrdinalIgnoreCase));
             return reading != null ? $"{reading.Value}{reading.Unit}" : "N/A";
         }
-
-        private string GetSensorReadingReference<T>(ISensor<T> sensor, string readingType) where T : class
-        {
-            var reading = sensor.ReadSensor().FirstOrDefault(r => r.Type.Equals(readingType, StringComparison.OrdinalIgnoreCase));
-            return reading != null ? $"{reading.Value}{reading.Unit}" : "N/A";
-        }
-
 
         /// <summary>
         /// Updates the health label based on sensor readings.
