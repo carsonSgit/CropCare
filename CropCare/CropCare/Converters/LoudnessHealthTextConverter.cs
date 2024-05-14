cfbff379e0b8b24e3cbf60deb7bf6c77859281ad
Cristiano Fazi <cristianofazi03@gmail.com>
2024-05-13 21:41:36 -0400

    Added loudness converter

---
+++ CropCare/CropCare/Converters/LoudnessHealthTextConverter.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Globalization;
+
+namespace CropCare.Converters
+{
+    public class LoudnessHealthTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string healthStatus;
+            try
+            {
+                if (value.ToString() == "Quiet")
+                    healthStatus = "Healthy";
+                else if (value.ToString() == "Noisy")
+                    healthStatus = "Caution";
+                else if (value.ToString() == "Loud")
+                    healthStatus = "Critical";
+                else
+                    healthStatus = "Unkown";
+            }
+            catch
+            {
+                healthStatus = "Unkown";
+            }
+
+            return healthStatus;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
